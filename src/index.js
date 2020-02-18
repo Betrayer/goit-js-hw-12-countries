60708a54663b4a69e906e39fe9a94ce4aa1ff721
Betrayer <StarDestroyer@ukr.net>
2020-02-18 14:53:38 +0200

    done

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import countryTemplate from './countryTemplate.hbs';
 import fetchCountries from './fetchCountries.js';
 import PNotify from '../node_modules/pnotify/dist/es/PNotify.js';
 import '../node_modules/pnotify/dist/PNotifyBrightTheme.css';
+import PNotifyButtons from 'pnotify/dist/es/PNotifyButtons';
 import debounce from 'lodash.debounce';
 import './styles.css';
 
 const inputForm = document.querySelector('input');
-inputForm.addEventListener('input', _.debounce(createTemplate, 500));
+inputForm.addEventListener('input', debounce(createTemplate, 500));
 const whereToAdd = document.querySelector('.countries__list');
 
 function createTemplate(event) {
@@ -16,23 +17,28 @@ function createTemplate(event) {
   }
   fetchCountries(event.target.value, countryResult);
 }
-
 function countryResult(array) {
   if (array.length >= 10) {
     PNotify.error({
       text: 'Try to be more specific!',
     });
   } else if (array.length > 1 && array.length <= 10) {
+    clearResults();
     whereToAdd.insertAdjacentHTML(
       'afterbegin',
       array.reduce((acc, element) => (acc += `<li>${element.name}</li>`), ''),
     );
     PNotify.closeAll();
   } else if (array.length === 1) {
+    clearResults();
     whereToAdd.insertAdjacentHTML(
       'afterbegin',
       array.reduce((acc, element) => (acc += countryTemplate(element)), ''),
     );
     PNotify.closeAll();
   }
+}
+
+function clearResults() {
+  whereToAdd.innerHTML = '';
 }
